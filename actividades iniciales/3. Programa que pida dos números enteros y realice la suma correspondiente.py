05b0cd96f718890f0bc5efe5890d73520c846bb8
Aldrich13758 <aldrich.agno@campus.xaloc.org>
2025-09-19 16:01:15 +0200

    Update 3. Programa que pida dos números enteros y realice la suma correspondiente.py

--- actividades iniciales/3. Programa que pida dos números enteros y realice la suma correspondiente.py
+++ actividades iniciales/3. Programa que pida dos números enteros y realice la suma correspondiente.py
@@ -1,4 +1,4 @@
 var1=int(input("introduce un número:"))
 var2=int(input("introduce otro número"))
 total=var1+var2
-print(f"el resultadode la suma de ambos números sería:", total)
+print(f"el resultado de la suma de ambos números sería:", total)
