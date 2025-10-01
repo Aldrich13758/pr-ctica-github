961b8268c3bc5355caa3376a8b6bbd5c156bbdd3
Aldrich13758 <aldrich.agno@campus.xaloc.org>
2025-10-01 09:50:12 +0200

    yu

---
+++ actividades condicionales/actividad23.py
@@ -0,0 +1,5 @@
+#23. Modifica el programa anterior para establecer si la nota es un excelente (8.5 a 10), un notable (>=6.5 -<8.5), satisfactorio (<6.5 -5) o insuficiente (<5). Controla que la nota introducida esté entre 0 y 10. Utilizar elif sin operadores lógicos.
+var1=float(input("introduce tu nota:"))
+if var1>8.5 and var
+    print("has sacado un excelente")
+elif var1
