5a2208a0a7e018277079ad8ae4cae13f9d609c5d
Aldrich13758 <aldrich.agno@campus.xaloc.org>
2026-01-30 15:16:10 +0100

    iui

--- actividades listas/actividad86.py
+++ actividades listas/actividad86.py
@@ -5,3 +5,7 @@ elif len(numero)!=8:
 #el signo de exclamación sirve para validar que la longitud del valor introducido es de 8 cifras
     print("Error: el número no tiene 8 cifras.") 
 else: print("Número válido")
+
+letras=["T","R","W","A","G","M","Y","F","P","D","X","B", "N","J","Z","S","Q","V","H","L","C","K","E"] 
+letra=letras[int(numero) % 23] 
+print(f"{numero}-{letra}")
