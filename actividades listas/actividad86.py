a81d78f3753a5ae1111a8179c04056c554d751b5
Aldrich13758 <aldrich.agno@campus.xaloc.org>
2026-02-04 09:24:47 +0100

    qq

--- actividades listas/actividad86.py
+++ actividades listas/actividad86.py
@@ -1,4 +1,4 @@
-numero=input("Introduce un número de 8 cifras: ") 
+numero=input("Introduce tu DNI: ") 
 if not numero.isdigit():
 #el .isdigit sirve para comprobar que el valor sea numerico 
     print("Error: el valor introducido no es numérico.")
@@ -12,15 +12,4 @@ letra=letras[int(numero) % 23]
 #operador % en Python se utiliza principalmente como operador de módulo,
 #que devuelve el residuo de la división entre dos números y así poder 
 #asignar una letra al valor introducido
-print(f"{numero}-{letra}")
-
-
-numero=input("Introduce un número de 8 cifras: ") 
-if not numero.isdigit(): 
-    print("Error: el valor introducido no es numérico.")
-elif len(numero)!=8:
-    print("Error: el número no tiene 8 cifras.") 
-else: print("Número válido")
-letras=["T","R","W","A","G","M","Y","F","P","D","X","B", "N","J","Z","S","Q","V","H","L","C","K","E"]
-letra=letras[int(numero) % 23]
 print(f"{numero}-{letra}")
