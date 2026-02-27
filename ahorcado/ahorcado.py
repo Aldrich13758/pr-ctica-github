5c99b1fead55e42ac2244531a06e9e0148cfce69
Aldrich13758 <aldrich.agno@campus.xaloc.org>
2026-02-27 15:54:12 +0100

    e

--- ahorcado/ahorcado.py
+++ ahorcado/ahorcado.py
@@ -1,14 +1,13 @@
 import random
-import time
-lista_palabras = ["python", "programacion", "ahorcado", "juego", "palabra", "secreta", "adivinar", "letras"]
-palabra_secreta = random.choice(lista_palabras).lower()
-letras_adivinadas = []
+lista_palabras=["python", "programacion", "ahorcado", "juego", "palabra", "secreta", "adivinar", "letras"]
+palabra_secreta=random.choice(lista_palabras).lower()
+letras_adivinadas=[]
 intentos=8                        
 print("¡Bienvenido al juego del Ahorcado!")
-print("La palabra secreta tiene", len(palabra_secreta)
-        , "letras. Tienes", intentos, "intentos para adivinarla.")
-while intentos > 0:
-    letra = input("Introduce una letra: ").lower()
+print("La palabra secreta tiene", len(palabra_secreta),"letras.")
+print("Tienes", intentos, "intentos para adivinarla.")
+while intentos>0:
+    letra=input("Introduce una letra: ").lower()
     if letra in letras_adivinadas:
         print("Ya has adivinado esa letra. Intenta con otra.")
         continue
@@ -18,12 +17,13 @@ while intentos > 0:
     else:
         intentos -= 1
         print("¡Incorrecto! Te quedan", intentos, "intentos.")
-    palabra_mostrada = ""
+        print(f"La palabra secreta era {palabra_secreta}.")
+    palabra_mostrada=""
     for letra_palabra in palabra_secreta:
         if letra_palabra in letras_adivinadas:
-            palabra_mostrada += letra_palabra + " "
+            palabra_mostrada+=letra_palabra + " "
         else:
-            palabra_mostrada += "_ "
+            palabra_mostrada+="_ "
     print(palabra_mostrada.strip())
     if "_" not in palabra_mostrada:
         print("¡Felicidades! Has adivinado la palabra secreta:", palabra_secreta)
